9dd5dfdf07519a264625f756c76a1cb8b8d7de66
Akayy <techakayy@gmail.com>
2023-10-11 10:45:12 +1100

    Minor cleanup

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,10 +2,10 @@ export default defineNuxtConfig({
   // ssr: false,
 
   modules: [
-    /* Module method */
     // 'vuetify-nuxt-module',
     '@nuxtjs/supabase',
   ],
+
   build: {
     transpile: ['vuetify'],
   },
