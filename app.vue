06b34aafc28fb520e010c5c51a051548058a04ad
Akayy <techakayy@gmail.com>
2023-10-11 09:51:03 +1100

    Minimal repro

--- app.vue
+++ app.vue
@@ -1,9 +1,8 @@
 <script setup>
-  import '~/assets/css/tailwind.css'
   // import { createClient } from '@supabase/supabase-js'
   // const supabase = createClient(
-  //   'https://vbxsosiulaeihtdkhkjh.supabase.co',
-  //   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZieHNvc2l1bGFlaWh0ZGtoa2poIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTY5NjE0MTgsImV4cCI6MjAxMjUzNzQxOH0.Kf_X1HLwb3JPeeM2h1QIs7-mn6BCkLBcMaI7Q60wIrs',
+  //   'https://test.supabase.co',
+  //   'test',
   // )
   // const countries = ref([])
 
@@ -21,15 +20,6 @@
     <!-- <ul>
       <li v-for="country in countries" :key="country.id">{{ country.name }}</li>
     </ul> -->
-    <NuxtLoadingIndicator />
-    <NuxtLayout>
-      <NuxtPage />
-    </NuxtLayout>
+    <v-card>Test</v-card>
   </div>
 </template>
-<style lang="scss">
-  // @use '~/assets/vuetify/settings';
-  // .my-button {
-  //   height: settings.$button-height;
-  // }
-</style>
--- types/image.ts
+++
@@ -1,8 +0,0 @@
-import { ImageSizes } from '@nuxt/image'
-
-export type ImageOptimized = {
-  imageSizes?: ImageSizes
-  imageSrc?: string
-  imageLazySrc?: string
-  bgStyles?: string
-}
